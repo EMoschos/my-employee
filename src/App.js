a0db61256b105c8e75328396a2adde8c2db231a2
Eleftherios Moschos <tmoschos@bigpond.com>
2020-09-22 23:27:29 +0930

    Updated App.js to work as class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { Component } from "react";
 import List from './components/List'
 import employees from './utils/employees.json'
 import './App.css';
 
-function App() {
-  return <List employees={employees} />;
-}
+class App extends Component {
+  state = {
+    employees
+  }
 
+  // sortEmployees = (role) => {
+  //   const sortEmployee;
+  // }
+
+  render() {
+    return (
+      <List employees={employees} />
+    );
+  }
+}
 export default App;
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -2,10 +2,8 @@ import React from "react";
 import { Table, Image } from 'react-bootstrap'
 import "./style.css";
 
-// Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 function List(props) {
-  // Using the filter method, we can create a new array containing only groceries which haven't been purchased
-  // const notPurchased = props.groceries.filter(grocery => !grocery.purchased);
+ 
   const employee = props.employees
 
   return (
@@ -34,31 +32,3 @@ function List(props) {
 }
 
 export default List;
-
-// {/* <ul className="list-group">
-//   {employee.map(item => (
-//     <li className="list-group-item" key={item.id}>
-//       {item.name}
-//     </li>
-//   ))}
-// </ul> */}
-
-// {/* <Card style={{ width: '18rem' }}>
-//   <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-//   <Card.Body>
-//     <Card.Title>Card Title</Card.Title>
-//     <Card.Text>
-//       Some quick example text to build on the card title and make up the bulk of
-//       the card's content.
-//     </Card.Text>
-//   </Card.Body>
-//   <ListGroup className="list-group-flush">
-//     <ListGroupItem>Cras justo odio</ListGroupItem>
-//     <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-//     <ListGroupItem>Vestibulum at eros</ListGroupItem>
-//   </ListGroup>
-//   <Card.Body>
-//     <Card.Link href="#">Card Link</Card.Link>
-//     <Card.Link href="#">Another Link</Card.Link>
-//   </Card.Body>
-// </Card> */}
