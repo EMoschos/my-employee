55bf9559ef769cf258321ccdc52743504dc7420e
Eleftherios Moschos <terrymoschos@gmail.com>
2020-09-23 22:49:20 +0000

    Updates

---
+++ precache-manifest.112ad3c5e42f9809f1e66ec42df51679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c742b14b7fe2f9117ec9de131ae56a34",
+    "url": "/my-employee/index.html"
+  },
+  {
+    "revision": "553932805ca9452c9e5f",
+    "url": "/my-employee/static/css/main.5db29773.chunk.css"
+  },
+  {
+    "revision": "ddf02fcd420d8670ab6a",
+    "url": "/my-employee/static/js/2.f025015f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-employee/static/js/2.f025015f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553932805ca9452c9e5f",
+    "url": "/my-employee/static/js/main.24d5e30f.chunk.js"
+  },
+  {
+    "revision": "b10bb7afd31faa3d9451",
+    "url": "/my-employee/static/js/runtime-main.b990d290.js"
+  }
+]);
